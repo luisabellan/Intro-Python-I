4c62389688804006654cb905b62e3567d5b98f81
luisabellan <luis.abellan.dev@gmail.com>
2020-06-05 00:19:11 +0200

    11_args.py done

--- src/11_args.py
+++ src/11_args.py
@@ -5,9 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-def f1_(a, b):
+def f1(a, b):
     return a + b
-
 
 
 print(f1(1, 2))
@@ -20,8 +19,11 @@ print(f1(1, 2))
 
 
 def f2(*nums):
+
     for num in nums:
-        return sum(nums+num)
+        if num is not None:
+            return sum(nums)
+    # print(sum(nums))
 
 
 print(f2(1))                    # Should print 1
@@ -32,14 +34,18 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
+
+def f3(a, b=1):
+    return a + b
 # YOUR CODE HERE
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -54,6 +60,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+
+def f4(**keys):
+    print(keys)
+
 
 # Should print
 # key: a, value: 12
@@ -72,4 +82,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
