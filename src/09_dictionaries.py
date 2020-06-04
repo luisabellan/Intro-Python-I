5276e621795e0b8437c6983a8a1bcf99711fb721
luisabellan <luis.abellan.dev@gmail.com>
2020-06-04 06:21:37 +0200

    09_dictionaries.py done

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,15 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+my_dict = {
+    "lat": 41,
+    "lon": -123,
+    "name": "Madrid"
+
+}
+waypoints.append(my_dict)
+# print(waypoints)
+
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +51,12 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = 'not a real place'
+#print(waypoints)
+
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for i in waypoints:
+    print(i)
