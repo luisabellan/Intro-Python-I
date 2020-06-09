acc3a3b846aa19ccc610d39de1388ee548d0faa6
Luis Abellan <luis.abellan.dev@gmail.com>
2020-06-09 05:23:15 +0200

    15_classes.py done

--- src/15_classes.py
+++ src/15_classes.py
@@ -1,5 +1,4 @@
 
-
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 # YOUR CODE HERE
@@ -10,6 +9,8 @@ class LatLon:
         self.lat = lat
         self.lon = lon
 
+    def __str__(self):
+        return (f"Latitude: {self.lat}\n Longitud: {self.lon}\n")
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
@@ -18,39 +19,50 @@ class LatLon:
 class Waypoint(LatLon):
     """ Waypoint class takes in latitude and longitud for a location """
 
-    def __init__(self, name, lat, lon):
 
+    def __init__(self, name, lat, lon):
         self.name = name
         super().__init__(lat, lon)
+
+    def __str__(self):
+        return (f"Name: {self.name}\n")
 
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 # YOUR CODE HERE
 
-class Geocache(LatLon, Waypoint):
+class Geocache(Waypoint):
     """ Add description here """
 
     def __init__(self, name, difficulty, size, lat, lon):
+
+
         self.difficulty = difficulty
         self.size = size
 
-        super().__init__(lat, lon, name)
+        super().__init__(name,lat,lon)
+
+    def __str__(self):
+        return (f"Name: {self.name}\n Difficulty: {self.difficulty}\n Size: {self.size}\n")
+
 
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 # YOUR CODE HERE
 
 waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+print(waypoint)
 
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
+
 print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
-
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 # Print it--also make this print more nicely
 print(geocache)
