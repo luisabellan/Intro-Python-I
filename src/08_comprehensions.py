bb895fb78ed1b8d44fbaa768f06af2eb1760fd3c
luisabellan <luis.abellan.dev@gmail.com>
2020-07-03 01:14:59 +0200

    deleted commented out code

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,7 +11,7 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = [i+1 for i in range(5)]
-print (y)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
@@ -37,6 +37,3 @@ x = input("Enter comma-separated numbers: ").split(',')
 y = [num for num in x if int(num) % 2 == 0]
 
 print(y)
-
-
-# y = [x.append(int(num)) for num in x if num % 2 == 0]
